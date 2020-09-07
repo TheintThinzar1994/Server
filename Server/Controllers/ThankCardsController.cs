8d52d1201263997694316ecb8953d27e0c6b3e7a
SoeSoeMMOCC <soesoemmocc@gmail.com>
2020-09-07 09:06:23 +0630

    thank_card_view_update

---
+++ Server/Controllers/ThankCardsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Server.Model;
+using Server.Services;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThankCardsController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+        private ThankCardsService _thankcardservice;
+
+        public ThankCardsController(ApplicationContext context,ThankCardsService thankcardservice)
+        {
+            _context = context;
+            _thankcardservice = thankcardservice;
+        }
+
+        // GET: api/ThankCards
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ThankCard>>> GetThankCards()
+        {
+            return await _context.ThankCards.ToListAsync();
+        }
+
+        // GET: api/ThankCards/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ThankCard>> GetThankCard(long? id)
+        {
+            var thankCard = await _context.ThankCards.FindAsync(id);
+
+            if (thankCard == null)
+            {
+                return NotFound();
+            }
+
+            return thankCard;
+        }
+
+        // PUT: api/ThankCards/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutThankCard(long? id, ThankCard thankCard)
+        {
+            if (id != thankCard.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(thankCard).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ThankCardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ThankCards
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<ThankCard>> PostThankCard(ThankCard thankCard)
+        {
+            _context.ThankCards.Add(thankCard);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetThankCard", new { id = thankCard.Id }, thankCard);
+        }
+
+        // DELETE: api/ThankCards/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ThankCard>> DeleteThankCard(long? id)
+        {
+            var thankCard = await _context.ThankCards.FindAsync(id);
+            if (thankCard == null)
+            {
+                return NotFound();
+            }
+
+            _context.ThankCards.Remove(thankCard);
+            await _context.SaveChangesAsync();
+
+            return thankCard;
+        }
+        [HttpPut]
+        [Route("UpdateView")]
+        public string UpdateSubDepartment(string paramList)
+        {
+            //Accepting data from 
+            var arr = JObject.Parse(paramList);
+            int id = (int)arr["Id"];
+            string status = (string)arr["status"];
+
+            //Checking Data Have or Not in ThanksCards Table
+            var thankcards = _context.ThankCards.Where(e => e.Id==id && e.isActive==true);
+
+            ////Creating Objects for Json Returns
+            IDictionary<string, List<object>> result = new Dictionary<string, List<object>>();
+            List<object> returndata = new List<object>();
+            List<object> returnstatus = new List<object>();
+            ReturnData retdata = new ReturnData();
+
+            //Checking Data Have or Not in ThankCards Table
+            if (thankcards.Count() > 0)
+            {
+                // Updating Row data with Client Update Data
+
+                List<object> thankcardlist = _thankcardservice.updateThankCardView(id,status);
+
+                //Return Updated Result to Client with JSON format
+                returndata.Add(thankcardlist);
+                retdata.statuscode = "200";
+                retdata.status = "Success";
+                returnstatus.Add(retdata);
+                result["status"] = returnstatus;
+                result["subdepartment"] = returndata;
+            }
+            else
+            {
+                // There is no data to update
+                retdata.statuscode = "304";
+                retdata.status = "No Data To Update";
+                returnstatus.Add(retdata);
+                result["status"] = returnstatus;
+                result["subdepartment"] = returndata;
+            }
+            return JsonConvert.SerializeObject(result);
+        }
+        private bool ThankCardExists(long? id)
+        {
+            return _context.ThankCards.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Server/Services/ThankCardsService.cs
@@ -0,0 +1,45 @@
+﻿using Server.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services
+{
+    public interface IThankCardsService
+    {
+        List<object> updateThankCardView(int id, string status);
+    }
+    public class ThankCardsService : IThankCardsService
+    {
+        private ApplicationContext _context;
+
+        public ThankCardsService(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public List<object> updateThankCardView(int id,string status)
+        {
+            List<ThankCard> thankcard = new List<ThankCard>();
+            var data1 = from s in _context.ThankCards
+                        where s.Id == id && s.isActive==true
+                        select s;
+            thankcard = data1.ToList<ThankCard>();
+            if (thankcard.Count() > 0)
+            {
+                ThankCard objthank = new ThankCard();
+                objthank = thankcard[0];
+                objthank.Status = status;
+                objthank.ts = DateTime.Now;
+                objthank.isActive = true;
+                _context.ThankCards.Update(objthank);
+                _context.SaveChanges();
+            }
+            data1 = from s in _context.ThankCards
+                    where s.Id == id && s.isActive == true
+                    select s;
+           List<object> ret_thankcard = data1.ToList<object>();
+            return ret_thankcard;
+        }
+    }
+}
