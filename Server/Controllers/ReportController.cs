4c6b41e43e5eb75888f94ec30106431fbbf5d519
SoeSoeMMOCC <soesoemmocc@gmail.com>
2020-09-08 13:08:13 +0630

    ssm_report_controller_updated

--- Server/Controllers/ReportController.cs
+++ Server/Controllers/ReportController.cs
@@ -105,6 +105,8 @@ namespace Server.Controllers
 
             return thankCard;
         }
+       
+        //Get Received ThankCard Total By Department -SSM 08/09/2020
         [HttpGet]
         [Route("GetThankCardTotalByDept")]
         public string getThankCardTotalByDepartment(string paramList)
@@ -115,6 +117,7 @@ namespace Server.Controllers
             string sub_dept_id = (string)arr["sub_dept_id"];
             DateTime from_date = (DateTime)arr["from_date"];
             DateTime to_date = (DateTime)arr["to_date"];
+            string order = (string)arr["order"];
 
             ////Creating Objects for Json Returns
             IDictionary<string, List<object>> result = new Dictionary<string, List<object>>();
@@ -124,7 +127,147 @@ namespace Server.Controllers
 
             //Getting Table Results from the Database
 
-            List<object> thankcard = _reportservice.getThankCardTotalByDepartment(dept_id,sub_dept_id, from_date, to_date);
+            List<object> thankcard = _reportservice.getThankCardTotalByDepartment(dept_id,sub_dept_id, from_date, to_date, order);
+
+            //Return Updated Result to Client with JSON format
+            returndata.Add(thankcard);
+            retdata.statuscode = "200";
+            retdata.status = "Success";
+            returnstatus.Add(retdata);
+            result["status"] = returnstatus;
+            result["thankcard"] = returndata;
+
+            return JsonConvert.SerializeObject(result);
+        }
+        
+        //Get Received ThankCard Total By Employee -- SSM 08/09/2020
+        [HttpGet]
+        [Route("GetThankCardTotalByEmployee")]
+        public string getReceivedThankCardTotalByEmployee(string paramList)
+        {
+            //Accepting data from             
+            var arr = JObject.Parse(paramList);
+            string dept_id = (string)arr["dept_id"];
+            string sub_dept_id = (string)arr["sub_dept_id"];
+            DateTime from_date = (DateTime)arr["from_date"];
+            DateTime to_date = (DateTime)arr["to_date"];
+            string to_emp_id = (string)arr["to_emp_id"];
+            string order = (string)arr["order"];
+
+            ////Creating Objects for Json Returns
+            IDictionary<string, List<object>> result = new Dictionary<string, List<object>>();
+            List<object> returndata = new List<object>();
+            List<object> returnstatus = new List<object>();
+            ReturnData retdata = new ReturnData();
+
+            //Getting Table Results from the Database
+
+            List<object> thankcard = _reportservice.getThankCardTotalByEmployee(dept_id, sub_dept_id, from_date, to_date,to_emp_id, order);
+
+            //Return Updated Result to Client with JSON format
+            returndata.Add(thankcard);
+            retdata.statuscode = "200";
+            retdata.status = "Success";
+            returnstatus.Add(retdata);
+            result["status"] = returnstatus;
+            result["thankcard"] = returndata;
+
+            return JsonConvert.SerializeObject(result);
+        }
+
+        //Get Received ThankCard Detail Data By Employee -- SSM 08/09/2020
+        [HttpGet]
+        [Route("GetThankCardTotalByEmployeeView")]
+        public string getReceivedThankCardTotalByEmployeeView(string paramList)
+        {
+            //Accepting data from             
+            var arr = JObject.Parse(paramList);
+            string dept_id = (string)arr["dept_id"];
+            string sub_dept_id = (string)arr["sub_dept_id"];
+            DateTime from_date = (DateTime)arr["from_date"];
+            DateTime to_date = (DateTime)arr["to_date"];
+            string to_emp_id = (string)arr["to_emp_id"];
+            string order = (string)arr["order"];
+
+            ////Creating Objects for Json Returns
+            IDictionary<string, List<object>> result = new Dictionary<string, List<object>>();
+            List<object> returndata = new List<object>();
+            List<object> returnstatus = new List<object>();
+            ReturnData retdata = new ReturnData();
+
+            //Getting Table Results from the Database
+
+            List<object> thankcard = _reportservice.getThankCardTotalByEmployeeView(dept_id, sub_dept_id, from_date, to_date, to_emp_id, order);
+
+            //Return Updated Result to Client with JSON format
+            returndata.Add(thankcard);
+            retdata.statuscode = "200";
+            retdata.status = "Success";
+            returnstatus.Add(retdata);
+            result["status"] = returnstatus;
+            result["thankcard"] = returndata;
+
+            return JsonConvert.SerializeObject(result);
+        }
+
+        //Get Sent ThankCard Total By Employee -- SSM 08/09/2020
+        [HttpGet]
+        [Route("GetSentThankCardTotalByEmployee")]
+        public string getSentThankCardTotalByEmployee(string paramList)
+        {
+            //Accepting data from             
+            var arr = JObject.Parse(paramList);
+            string dept_id = (string)arr["dept_id"];
+            string sub_dept_id = (string)arr["sub_dept_id"];
+            DateTime from_date = (DateTime)arr["from_date"];
+            DateTime to_date = (DateTime)arr["to_date"];
+            string from_emp_id = (string)arr["from_emp_id"];
+            string order = (string)arr["order"];
+
+            ////Creating Objects for Json Returns
+            IDictionary<string, List<object>> result = new Dictionary<string, List<object>>();
+            List<object> returndata = new List<object>();
+            List<object> returnstatus = new List<object>();
+            ReturnData retdata = new ReturnData();
+
+            //Getting Table Results from the Database
+
+            List<object> thankcard = _reportservice.getSentThankCardTotalByEmployee(dept_id, sub_dept_id, from_date, to_date, from_emp_id, order);
+
+            //Return Updated Result to Client with JSON format
+            returndata.Add(thankcard);
+            retdata.statuscode = "200";
+            retdata.status = "Success";
+            returnstatus.Add(retdata);
+            result["status"] = returnstatus;
+            result["thankcard"] = returndata;
+
+            return JsonConvert.SerializeObject(result);
+        }
+
+        //Get Received ThankCard Detail Data By Employee -- SSM 08/09/2020
+        [HttpGet]
+        [Route("GetSentThankCardTotalByEmployeeView")]
+        public string getSentThankCardTotalByEmployeeView(string paramList)
+        {
+            //Accepting data from             
+            var arr = JObject.Parse(paramList);
+            string dept_id = (string)arr["dept_id"];
+            string sub_dept_id = (string)arr["sub_dept_id"];
+            DateTime from_date = (DateTime)arr["from_date"];
+            DateTime to_date = (DateTime)arr["to_date"];
+            string from_emp_id = (string)arr["from_emp_id"];
+            string order = (string)arr["order"];
+
+            ////Creating Objects for Json Returns
+            IDictionary<string, List<object>> result = new Dictionary<string, List<object>>();
+            List<object> returndata = new List<object>();
+            List<object> returnstatus = new List<object>();
+            ReturnData retdata = new ReturnData();
+
+            //Getting Table Results from the Database
+
+            List<object> thankcard = _reportservice.getSentThankCardTotalByEmployeeView(dept_id, sub_dept_id, from_date, to_date, from_emp_id, order);
 
             //Return Updated Result to Client with JSON format
             returndata.Add(thankcard);
