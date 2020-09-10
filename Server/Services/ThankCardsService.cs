673822f2fb26d9b5396267003c30bdb97a47f14c
SuNandar86 <sunandarmmocc@gmail.com>
2020-09-09 20:07:47 -0700

    edit in ThankCard Service

--- Server/Services/ThankCardsService.cs
+++ Server/Services/ThankCardsService.cs
@@ -144,7 +144,8 @@ namespace Server.Services
                             Dept_Name = d.Name,
                             Sub_Dept_Name = sd.Name,
                             Date = tc.SendDate,
-                            Status = tc.Status
+                            Status = tc.Status,
+                            Thank_Id =tc.Id
                         };
             retdata = data1.ToList<object>();
             return retdata;
